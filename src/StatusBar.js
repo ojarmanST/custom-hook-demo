e14c908c99f871f9485c3bb834dfd9f52d6a4616
Olivia Jarman <admin@Olivias-MBP-2.attlocal.net>
2023-05-04 09:50:56 -0400

    structure initialized, custom hook wip

---
+++ src/StatusBar.js
@@ -0,0 +1,5 @@
+import useOnlineStatus from "./useOnlineStatus";
+export default function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+}
